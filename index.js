28f1ddd0fd79856f73f8d85b593e7853bf355bde
hesamuddin.sayed <hesamuddin.sayed@gmail.com>
2024-12-24 12:09:03 -0500

    Basic functionality completed

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const randomButton = document.getElementById("bttn");
+const randomButton = document.getElementById("next_challenger");
+const resetRun = document.getElementById("reset")
+const stopWatchDisplay = document.getElementById("stopwatch_display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
 
-const characterList = [
+let characterList = [
     "Reina",
     "Victor Chevalier",
     "Panda",
@@ -40,11 +46,51 @@ const characterList = [
     ]
 
 randomButton.addEventListener("click", function () {
+    if(!isRunning) {
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+    }
     const randomChar = characterList[Math.floor(Math.random() * characterList.length)]
     document.getElementById("character_name").innerHTML = randomChar
     characterList.splice((characterList.indexOf(randomChar)), 1)
-    console.log(characterList)
     if (characterList.length === 0) {
         document.getElementById("character_name").innerHTML = "Congratulations you have completed the Ironman Challenge!"
     }
+    if (isRunning && characterList.length === 0) {
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning = false;
+    }
 })
+
+resetRun.addEventListener("click", function () {
+    if (isRunning) {
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning =false;
+    }
+    if (confirm ("Are you sure you want to reset your run?")) {
+        location.reload();
+    }
+    startTime = Date.now() - elapsedTime;
+    timer = setInterval(update, 10);
+    isRunning = true;
+})
+
+function update() {
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10)
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    stopWatchDisplay.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`
+}
